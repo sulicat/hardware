0388c579fec236c6600e2428e6c384c4315c1949
Suleiman Barakat <suli.barakat@gmail.com>
2025-04-14 21:45:57 -0400

    Basic HID output

--- KeyStick/software/V1/KeyStick/build/esp-idf/cmake_install.cmake
+++ KeyStick/software/V1/KeyStick/build/esp-idf/cmake_install.cmake
@@ -564,6 +564,16 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("/home/sulicat/git/hardware/KeyStick/software/V1/KeyStick/build/esp-idf/espressif__tinyusb/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/sulicat/git/hardware/KeyStick/software/V1/KeyStick/build/esp-idf/espressif__esp_tinyusb/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("/home/sulicat/git/hardware/KeyStick/software/V1/KeyStick/build/esp-idf/main/cmake_install.cmake")
 endif()
 
--- KeyStick/software/V1/KeyStick/main/KeyStick.c
+++ KeyStick/software/V1/KeyStick/main/KeyStick.c
@@ -6,6 +6,78 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "tinyusb.h"
+#include "class/hid/hid_device.h"
+#include "driver/gpio.h"
+
+#define TUSB_DESC_TOTAL_LEN (TUD_CONFIG_DESC_LEN + CFG_TUD_HID * TUD_HID_DESC_LEN)
+
+const char *hid_string_descriptor[5] = {
+    // array of pointer to string descriptors
+    (char[]){0x09, 0x04},    // 0: is supported language is English (0x0409)
+    "TinyUSB",               // 1: Manufacturer
+    "TinyUSB Device",        // 2: Product
+    "123456",                // 3: Serials, should use chip ID
+    "Example HID interface", // 4: HID
+};
+
+const uint8_t hid_report_descriptor[] = {
+    TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(HID_ITF_PROTOCOL_KEYBOARD)),
+    TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(HID_ITF_PROTOCOL_MOUSE))};
+
+static const uint8_t hid_configuration_descriptor[] = {
+    // Configuration number, interface count, string index, total length, attribute, power in mA
+    TUD_CONFIG_DESCRIPTOR(1, 1, 0, TUSB_DESC_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+    // Interface number, string index, boot protocol, report descriptor len, EP In address, size & polling interval
+    TUD_HID_DESCRIPTOR(0, 4, false, sizeof(hid_report_descriptor), 0x81, 16, 10),
+};
+
+void init_usb() {
+
+    const tinyusb_config_t tusb_cfg = {
+        .device_descriptor = NULL,
+        .string_descriptor = hid_string_descriptor,
+        .string_descriptor_count = sizeof(hid_string_descriptor) / sizeof(hid_string_descriptor[0]),
+        .external_phy = false,
+        .configuration_descriptor = hid_configuration_descriptor,
+    };
+
+    tinyusb_driver_install(&tusb_cfg);
+}
+
+
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance)
+{
+    // We use only one interface and one HID report descriptor, so we can ignore parameter 'instance'
+    return hid_report_descriptor;
+}
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
+{
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+
+    return 0;
+}
+
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize) {}
+
+void step_usb() {
+    if (tud_mounted()) {
+        printf("Sending Keyboard report\n");
+        uint8_t keycode[6] = {HID_KEY_A};
+        tud_hid_keyboard_report(HID_ITF_PROTOCOL_KEYBOARD, 0, keycode);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        tud_hid_keyboard_report(HID_ITF_PROTOCOL_KEYBOARD, 0, NULL);
+    }
+}
+
 void app_main(void) {
 
     gpio_config_t io_conf = {};
@@ -18,6 +90,7 @@ void app_main(void) {
     io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
 
+    init_usb();
 
     int counter = 0;
     while (1) {
@@ -28,6 +101,8 @@ void app_main(void) {
         counter += 1;
         counter = counter % 3;
 
-        vTaskDelay(pdMS_TO_TICKS(500));
+        step_usb();
+
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
